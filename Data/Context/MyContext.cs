5df40995ff3793526d3e8afb54d0811553e5364f
narba <narba@DESKTOP-KRRRREC>
2022-11-11 14:45:21 -0300

    Add project files.

---
+++ Data/Context/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public MyContext (DbContextOptions<MyContext> options) : base(options) 
+        { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
