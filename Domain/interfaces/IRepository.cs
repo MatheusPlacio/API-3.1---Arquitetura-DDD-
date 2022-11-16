e5f18dc01e96d4c65159a7ce5bb7dabc6962ef9a
narba <narba@DESKTOP-KRRRREC>
2022-11-16 17:37:12 -0300

    FirstMigration

---
+++ Domain/interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+
+    }
+}
